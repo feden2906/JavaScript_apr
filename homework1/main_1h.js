636a6c72ab95f8f9f6d4c547528603536e3fde46
IgorChuprina <79160579+IgorChuprina@users.noreply.github.com>
2021-06-03 01:11:47 +0300

    Homework_1

--- homework1/main_1h.js
+++ homework1/main_1h.js
@@ -59,3 +59,88 @@ str1 = 'мy';
 str2 = 'name';
 str3 = 'is';
 str4 = 'Ihor';
+num1 = NaN;
+num2 = 555;
+num3 = -523;
+num4 = 5.56;
+num5 = 1000;
+num6 = '25';
+num7 = 9.56;
+bul1 = false;
+bul2 = true;
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+console.log(bul1);
+console.log(bul2);
+alert(str1);
+alert(str2);
+alert(str3);
+alert(str4);
+alert(num1);
+alert(num2);
+alert(num3);
+alert(num4);
+alert(num5);
+alert(num6);
+alert(num7);
+alert(bul1);
+alert(bul2);
+document.write(str1);
+document.write(str2);
+document.write(str3);
+document.write(str4);
+document.write(num1);
+document.write(num2);
+document.write(num3);
+document.write(num4);
+document.write(num5);
+document.write(num6);
+document.write(num7);
+document.write(bul1);
+document.write(bul2);
+
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. Зконкатенувати їх в одну змінну person.
+let firstName = 'Ігор';
+let middleName = 'Вікторович';
+let lastName = 'Чуприна';
+let person = (`${lastName} ${firstName} ${middleName}`);
+
+// За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+let firstName1 = prompt('Hello!! Enter name');
+let middleName1 = prompt('Enter middle name');
+let age = +prompt('Enter age');
+alert(`Вітаю ${firstName1} ${middleName1}. Тобі ${age}`);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+// В однакових виразаї не використовувати однакові оператори!!!
+// 5 < 6 -> true
+// 5 > 6 -> false
+// 5 == 6 -> false
+// 5 >= 6 -> false
+// 10 == 10 -> true
+// 10 === 10 -> true
+// 10 > 10 -> false
+// 10 < 10 -> false
+// 10 != 10 -> false
+// 123 === '123' -> false
+// 123 == '123' -> true
+
+
