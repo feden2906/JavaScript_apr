9d844fefa9f91377c00069b59942ccba93444294
IgorChuprina <79160579+IgorChuprina@users.noreply.github.com>
2021-06-02 18:36:51 +0300

    Homework_1

---
+++ homework1/main_1h.js
@@ -0,0 +1,61 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+let str1 = 'hello';
+let str2 = 'owu';
+let str3 = 'com';
+let str4 = 'ua';
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let num5 = 3.14;
+let num6 = 2.7;
+let num7 = 16;
+let bul1 = true;
+let bul2 = false;
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+console.log(bul1);
+console.log(bul2);
+alert(str1);
+alert(str2);
+alert(str3);
+alert(str4);
+alert(num1);
+alert(num2);
+alert(num3);
+alert(num4);
+alert(num5);
+alert(num6);
+alert(num7);
+alert(bul1);
+alert(bul2);
+document.write(str1);
+document.write(str2);
+document.write(str3);
+document.write(str4);
+document.write(num1);
+document.write(num2);
+document.write(num3);
+document.write(num4);
+document.write(num5);
+document.write(num6);
+document.write(num7);
+document.write(bul1);
+document.write(bul2);
+
+// - Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+str1 = 'мy';
+str2 = 'name';
+str3 = 'is';
+str4 = 'Ihor';
